b661642d6f57ca2191425ed8a58dc80ffaad8f06
Stephen Hendrix <StephenTHendrix@gmail.com>
2025-07-06 13:20:32 -0500

    prove shadcn card on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { Card } from "@/components/ui/card";
+
 export default function Home() {
   return (
+    <Card>
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+    </Card>
   )
 }
