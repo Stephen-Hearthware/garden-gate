d123abc79f651954489ac448116185e6c45d71d3
Stephen Hendrix <StephenTHendrix@gmail.com>
2025-07-06 13:33:53 -0500

    install, configure, and run prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import { Card } from "@/components/ui/card";
 export default function Home() {
   return (
     <Card>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </Card>
-  )
+  );
 }
